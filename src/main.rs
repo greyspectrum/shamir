50ec96287d493b54f8043261675af10c681fd9e9
greyspectrum <hello@greyspectrum.io>
2022-03-10 20:47:16 -0500

    don't use depreciated syntax

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::io;
-extern crate threshold_secret_sharing as tss;
+use threshold_secret_sharing as tss;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
